6451f883106a72200868b6ba2fa163331de0e945
RedMoon4 <53101643+RedMoon4@users.noreply.github.com>
2019-09-17 11:20:21 -0700

    First Assignment

---
+++ ExpressionAssignment-master/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String args[]){
+        /*For the following expressions write out using code step by step how the
+        compiler will evaluate it
+        Example:
+        System.out.println(2 * 5 + 3);
+        System.out.println(10 + 3);
+        System.out.println(13);
+        Only complete one computation per line of code
+        The output for every line is always equal to the same number
+        In example above the out put should be
+        13
+        13
+        13
+        */
+        //Expression 1
+        System.out.println((2.0 + 4) * 8/2);
+
+        //Expression 2
+        System.out.println(Math.sqrt(576));
+
+        //Expression 3
+        System.out.println(36.0 * 4/6);
+
+        //Expression 4
+        System.out.println(Math.sqrt(Math.pow(2, 4) * Math.pow(2,3) * 4.5));
+
+        //Expression 5
+        System.out.println((Math.sqrt(Math.pow(6, 8)) - (720)) / 24);
+    }
+}
